1304b334514c15fecf86b9abd4e16f50b8d88ce1
Nikapwente <hrsmajor@gmail.com>
2023-12-13 09:48:59 +0300

    Assignment Complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -2,3 +2,36 @@
 
 class MyString:
   pass
+  def __init__(self, value=""):
+    self.value=value
+    
+  def get_value(self):
+    return self._value
+
+  def set_value(self, value=""):
+    if (type(value) == (str)):
+      self._value = value
+    else:
+      print("The value must be a string.")
+  
+  value = property(get_value, set_value,)
+
+  def is_sentence(self):
+    return (self.value[(len(self.value)-1)] == ".")
+
+  def is_question(self):
+    return (self.value[(len(self.value)-1)] == "?")
+
+  def is_exclamation(self):
+    return (self.value[(len(self.value)-1)] == "!")
+  
+  def count_sentences(self):
+    sentence_endings = ["!", "?", "."]
+    num_sentences = 0
+    for char in range(len(self.value)):
+      if self.value[char] in sentence_endings and (self.value[char] != self.value[char-1]):
+        num_sentences += 1
+        print(self.value[char])
+    return num_sentences
+
+
